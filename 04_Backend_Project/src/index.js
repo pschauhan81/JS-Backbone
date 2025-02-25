4210f046c0b30425b4b5a68a3b3f505ef5df8d45
pschauhan81 <ps8168372@gmail.com>
2025-02-25 11:28:51 +0530

    databse connect one type

--- 04_Backend_Project/src/index.js
+++ 04_Backend_Project/src/index.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import {DB_NAME} from "./constants";
+
+
 import express from "express";
 const app = express()
 
@@ -11,7 +13,9 @@ const app = express()
             throw error
         })
 
-        
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
 
     }catch(error){
         console.error("ERROR",error)
