e14be5b98c8470265d3f76f7d8bd1c60ca101d10
pschauhan81 <ps8168372@gmail.com>
2025-02-27 13:22:21 +0530

    custom api

--- 04_Backend_Project/src/app.js
+++ 04_Backend_Project/src/app.js
@@ -9,7 +9,10 @@ app.use(cors({
     credentials:true
 }))
 
-
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true, limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
 
 export { app }
