7354d15413e46616d160f3b6449cfc49f481a376
pschauhan81 <ps8168372@gmail.com>
2025-03-03 13:10:00 +0530

    multer

--- 04_Backend_Project/src/utils/cloudinary.js
+++ 04_Backend_Project/src/utils/cloudinary.js
@@ -26,8 +26,9 @@ const uploadOnCloudinary = async(localFilePath) =>{
 
 export{uploadOnCloudinary}
 
-
-
+cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikimedia/commons/a/ae/olympic_flag.jpg",
+{public_id:"olympic_flag"},
+function(error,result) {console.log(result); }  )
 
 
 
