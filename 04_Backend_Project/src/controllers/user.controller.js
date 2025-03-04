fac0b4e09c779bb5d0ff2868d9db3d18cdaeff24
pschauhan81 <ps8168372@gmail.com>
2025-03-04 14:57:04 +0530

    space mainten

--- 04_Backend_Project/src/controllers/user.controller.js
+++ 04_Backend_Project/src/controllers/user.controller.js
@@ -1,11 +1,11 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    message: "ok",
+  });
+});
 
-const registerUser = asyncHandler( async (req, res)=>{
-    res.status(200).json({
-        message:"ok"
-    })
-}) 
-
-
-export {registerUser}
+export {
+     registerUser
+    };
